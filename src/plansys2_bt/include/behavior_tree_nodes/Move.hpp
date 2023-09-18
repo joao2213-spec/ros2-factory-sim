19bcce065dd847f764a774a352f91373f99bc26d
joao2213-spec <101711755+joao2213-spec@users.noreply.github.com>
2023-09-18 09:55:34 +0100

    Simple move command

--- src/plansys2_bt/include/behavior_tree_nodes/Move.hpp
+++ src/plansys2_bt/include/behavior_tree_nodes/Move.hpp
@@ -2,6 +2,11 @@
 #define PLANSYS2_BT__BEHAVIOR_TREE_NODES__MOVE_HPP_
 
 #include <string>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
 
@@ -13,10 +18,23 @@ class Move : public BT::SyncActionNode
 public:
   Move(
     const std::string & name,
-    const BT::NodeConfiguration & config)
-    : BT::SyncActionNode(name, config) {}
+    const BT::NodeConfiguration & config);
 
   BT::NodeStatus tick() override;
+
+  static BT::PortsList providedPorts()
+  {
+    return {
+      BT::InputPort<std::string>("robot"),
+      BT::InputPort<std::string>("goal")
+    };
+  }
+
+private:
+  rclcpp::Node::SharedPtr node_;
+  rclcpp::NodeOptions options_;
+  rclcpp::executors::SingleThreadedExecutor executor_;
+  std::shared_ptr<rclcpp_action::Client<nav2_msgs::action::NavigateToPose>> action_client_;
 };
 
 }  // namespace plansys2_bt_tests
--- src/plansys2_bt/src/behavior_tree_nodes/Move.cpp
+++ src/plansys2_bt/src/behavior_tree_nodes/Move.cpp
@@ -1,29 +1,72 @@
-#include <iostream>
 #include "behavior_tree_nodes/Move.hpp"
 
 namespace plansys2_bt_tests
 {
 
-BT::NodeStatus
-Move::tick()
+Move::Move(
+  const std::string & name,
+  const BT::NodeConfiguration & config)
+: BT::SyncActionNode(name, config), node_(rclcpp::Node::make_shared("move_action_node"))
 {
-  std::cout << "Hello, World!" << std::endl;
-  return BT::NodeStatus::SUCCESS;
+  action_client_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(
+    node_, "/carter1/navigate_to_pose");
+}
+
+BT::NodeStatus Move::tick()
+{
+  std::string robot;
+  getInput<std::string>("robot", robot);
+
+  std::string goal_str;
+  getInput<std::string>("goal", goal_str);
+
+  std::vector<std::string> coords;
+  std::istringstream iss(goal_str);
+  std::string s;
+  while (getline(iss, s, ',')) {
+    coords.push_back(s);
+  }
+
+  if (coords.size() != 2) {
+    return BT::NodeStatus::FAILURE;
+  }
+
+  double goal_x = std::stod(coords[0]);
+  double goal_y = std::stod(coords[1]);
+
+  nav2_msgs::action::NavigateToPose::Goal goal_msg;
+  goal_msg.pose.header.frame_id = "map";
+  goal_msg.pose.pose.position.x = goal_x;
+  goal_msg.pose.pose.position.y = goal_y;
+  goal_msg.pose.pose.orientation.w = 1.0;
+
+  auto future_goal_handle = action_client_->async_send_goal(goal_msg);
+  if (rclcpp::spin_until_future_complete(node_, future_goal_handle) !=
+    rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
+    return BT::NodeStatus::FAILURE;
+  }
+
+  auto goal_handle = future_goal_handle.get();
+  if (!goal_handle) {
+    return BT::NodeStatus::FAILURE;
+  }
+
+  auto future_result = action_client_->async_get_result(goal_handle);
+  rclcpp::spin_until_future_complete(node_, future_result);
+
+  auto result = future_result.get();
+  if (result.code == rclcpp_action::ResultCode::SUCCEEDED) {
+    return BT::NodeStatus::SUCCESS;
+  } else {
+    return BT::NodeStatus::FAILURE;
+  }
 }
 
 }  // namespace plansys2_bt_tests
 
 #include "behaviortree_cpp_v3/bt_factory.h"
-
-BT::NodeBuilder getNodeBuilderForMove()
-{
-    return [](const std::string& name, const BT::NodeConfiguration& config) -> std::unique_ptr<BT::TreeNode> {
-        return std::make_unique<plansys2_bt_tests::Move>(name, config);
-    };
-}
-
 BT_REGISTER_NODES(factory)
 {
-    factory.registerBuilder<plansys2_bt_tests::Move>("Move", getNodeBuilderForMove());
+  factory.registerNodeType<plansys2_bt_tests::Move>("Move");
 }
-
