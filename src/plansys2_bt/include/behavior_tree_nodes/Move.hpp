b542182326f0e06aa87f4a88ea9bdb05f91821f2
joao2213-spec <101711755+joao2213-spec@users.noreply.github.com>
2023-09-18 08:03:17 +0100

    Hello world behaviour tree example

--- src/plansys2_bt/include/behavior_tree_nodes/Move.hpp
+++ src/plansys2_bt/include/behavior_tree_nodes/Move.hpp
@@ -2,40 +2,21 @@
 #define PLANSYS2_BT__BEHAVIOR_TREE_NODES__MOVE_HPP_
 
 #include <string>
-#include <map>
-
-#include "geometry_msgs/msg/pose2_d.hpp"
-#include "nav2_msgs/action/navigate_to_pose.hpp"
-
-#include "plansys2_bt_actions/BTActionNode.hpp"
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
 
 namespace plansys2_bt_tests
 {
 
-class Move : public plansys2::BtActionNode<
-    nav2_msgs::action::NavigateToPose>
+class Move : public BT::SyncActionNode
 {
 public:
-  explicit Move(
-    const std::string & xml_tag_name,
-    const std::string & action_name,
-    const BT::NodeConfiguration & conf);
+  Move(
+    const std::string & name,
+    const BT::NodeConfiguration & config)
+    : BT::SyncActionNode(name, config) {}
 
-  void on_tick() override;
-  BT::NodeStatus on_success() override;
-
-  static BT::PortsList providedPorts()
-  {
-    return {
-      BT::InputPort<std::string>("goal")
-    };
-  }
-
-private:
-  int goal_reached_;
-  std::map<std::string, geometry_msgs::msg::Pose2D> waypoints_;
+  BT::NodeStatus tick() override;
 };
 
 }  // namespace plansys2_bt_tests
